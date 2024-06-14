735f9ef2913b56baf748e624dd0cd13aa660218f
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-06-14 22:47:34 +0700

    Feat: Add get contacts fetching component

---
+++ lib/data.ts
@@ -0,0 +1,8 @@
+export const getContacts = async () => {
+  try {
+    const contacts = await prisma?.contact.findMany();
+    return contacts;
+  } catch (error) {
+    throw new Error('Failed to fetch contact data');
+  }
+};
