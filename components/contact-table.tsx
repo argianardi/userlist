bc8089bfa86cfdd2049d08600ad08d201ef785ba
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-06-15 20:37:34 +0700

    Feat: Fetch contact list from data base

--- components/contact-table.tsx
+++ components/contact-table.tsx
@@ -1,6 +1,11 @@
+import { getContacts } from '@/lib/data';
+import { formatDate } from '@/lib/utils';
 import React from 'react';
+import { DeleteButton, EditButton } from './button';
 
-const ContactTable = () => {
+const ContactTable = async () => {
+  const contacts = await getContacts();
+
   return (
     <table className="w-full text-sm text-left text-gray-500">
       <thead className="text-sm text-gray-700 uppercase bg-gray-50">
@@ -13,13 +18,20 @@ const ContactTable = () => {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td className="py-3 px-6"></td>
-          <td className="py-3 px-6"></td>
-          <td className="py-3 px-6"></td>
-          <td className="py-3 px-6"></td>
-          <td className="py-3 px-6"></td>
-        </tr>
+        {contacts?.map((contact, index) => (
+          <tr key={index}>
+            <td className="py-3 px-6">{index + 1}</td>
+            <td className="py-3 px-6">{contact?.name}</td>
+            <td className="py-3 px-6">{contact?.phone}</td>
+            <td className="py-3 px-6">
+              {formatDate(contact?.createAt.toString())}
+            </td>
+            <td className="py-3 flex justify-center gap-1">
+              <EditButton />
+              <DeleteButton />
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
