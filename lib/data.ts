7b52c95aa4e30593a3e0a7e34077eae20a3fae56
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-06-16 11:22:49 +0700

    Feat: Add fetch data by id method

--- lib/data.ts
+++ lib/data.ts
@@ -1,9 +1,39 @@
 import { prisma } from '@/lib/prisma';
 
+// Get all contacts
 export const getContacts = async () => {
   try {
-    const contacts = await prisma?.contact.findMany();
+    const contacts = await prisma?.contact
+      .findMany({
+        orderBy: { name: 'asc' },
+      })
+      .then((contacts) =>
+        contacts.sort((firstContact, secondContact) => {
+          if (
+            firstContact.name.toLowerCase() < secondContact.name.toLowerCase()
+          )
+            return -1;
+          if (
+            firstContact.name.toLowerCase() > secondContact.name.toLowerCase()
+          )
+            return 1;
+          return 0;
+        })
+      );
+
     return contacts;
+  } catch (error) {
+    throw new Error('Failed to fetch contact data');
+  }
+};
+
+// Get contact by id
+export const getContactById = async (id: string) => {
+  try {
+    const contact = await prisma?.contact.findUnique({
+      where: { id },
+    });
+    return contact;
   } catch (error) {
     throw new Error('Failed to fetch contact data');
   }
