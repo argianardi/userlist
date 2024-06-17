ffdd3331dffc79b14ff4b049201ceac33298a0b9
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-06-17 13:44:40 +0700

    Feat: Catch page param and add pagination component

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,16 +1,29 @@
 import { CreateButton } from '@/components/button';
 import ContactTable from '@/components/contact-table';
+import Pagination from '@/components/pagination';
 import Search from '@/components/search';
+import { getContactPages } from '@/lib/data';
 import React from 'react';
 
-const Contacts = () => {
+const Contacts = async ({
+  searchParams,
+}: {
+  searchParams?: { 'search-contact'?: string; page?: string };
+}) => {
+  const query = searchParams?.['search-contact'] || '';
+  const totalPages = await getContactPages(query);
+  const currentPage = Number(searchParams?.page) || 1;
+
   return (
     <div className="max-w-screen-md mx-auto mt-5">
       <div className="flex items-center justify-between gap-1 mb-5">
         <Search />
         <CreateButton />
       </div>
-      <ContactTable />
+      <ContactTable query={query} currentPage={currentPage} />
+      <div className="flex justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
     </div>
   );
 };
