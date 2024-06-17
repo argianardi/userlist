d778067d7a6b7575e396cdc1c12d630ba61275b5
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-06-17 13:49:56 +0700

    Feat: Add generate pagination method

--- lib/utils.ts
+++ lib/utils.ts
@@ -14,3 +14,27 @@ export const formatDate = (dateStr: string) => {
   // Format objek Date menggunakan formatter dan kembalikan string tanggal
   return formatter.format(date);
 };
+
+export const generatePagination = (currentPage: number, totalPages: number) => {
+  if (totalPages <= 7) {
+    return Array.from({ length: totalPages }, (_, i) => i + 1);
+  }
+
+  if (currentPage <= 3) {
+    return [1, 2, 3, '...', totalPages];
+  }
+
+  if (currentPage >= totalPages - 2) {
+    return [1, 2, 3, '...', totalPages - 2, totalPages - 1, totalPages];
+  }
+
+  return [
+    1,
+    '...',
+    currentPage - 1,
+    currentPage,
+    currentPage + 1,
+    '...',
+    totalPages,
+  ];
+};
