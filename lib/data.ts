c53dbc4bcee9c616a72db2a3c8a8d9dd18259215
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-06-17 13:49:33 +0700

    Feat: Add contact limit per page

--- lib/data.ts
+++ lib/data.ts
@@ -1,27 +1,43 @@
 import { prisma } from '@/lib/prisma';
 
-// Get all contacts
-export const getContacts = async () => {
-  try {
-    const contacts = await prisma?.contact
-      .findMany({
-        orderBy: { name: 'asc' },
-      })
-      .then((contacts) =>
-        contacts.sort((firstContact, secondContact) => {
-          if (
-            firstContact.name.toLowerCase() < secondContact.name.toLowerCase()
-          )
-            return -1;
-          if (
-            firstContact.name.toLowerCase() > secondContact.name.toLowerCase()
-          )
-            return 1;
-          return 0;
-        })
-      );
+const ITEMS_PER_PAGE = 5;
 
-    return contacts;
+// Get all contacts
+export const getContacts = async (query: string, currentPage: number) => {
+  const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+
+  try {
+    const contacts = await prisma?.contact.findMany({
+      where: {
+        OR: [
+          {
+            name: {
+              contains: query,
+              mode: 'insensitive',
+            },
+          },
+          {
+            phone: {
+              contains: query,
+              mode: 'insensitive',
+            },
+          },
+        ],
+      },
+      orderBy: {
+        name: 'asc',
+      },
+      take: ITEMS_PER_PAGE,
+      skip: offset,
+    });
+
+    return contacts.sort((firstContact, secondContact) => {
+      if (firstContact.name.toLowerCase() < secondContact.name.toLowerCase())
+        return -1;
+      if (firstContact.name.toLowerCase() > secondContact.name.toLowerCase())
+        return 1;
+      return 0;
+    });
   } catch (error) {
     throw new Error('Failed to fetch contact data');
   }
@@ -34,6 +50,34 @@ export const getContactById = async (id: string) => {
       where: { id },
     });
     return contact;
+  } catch (error) {
+    throw new Error('Failed to fetch contact data');
+  }
+};
+
+// Get contact pages
+export const getContactPages = async (query: string) => {
+  try {
+    const contacts = await prisma?.contact.count({
+      where: {
+        OR: [
+          {
+            name: {
+              contains: query,
+              mode: 'insensitive',
+            },
+          },
+          {
+            phone: {
+              contains: query,
+              mode: 'insensitive',
+            },
+          },
+        ],
+      },
+    });
+    const totalPages = Math.ceil(Number(contacts) / ITEMS_PER_PAGE);
+    return totalPages;
   } catch (error) {
     throw new Error('Failed to fetch contact data');
   }
